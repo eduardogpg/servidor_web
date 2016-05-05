e62dfede6bee4d8d1072d9dab8e0f4143953023a
Eduardo Ismael <eduardo78d@gmail.com>
2016-05-04 21:26:03 -0500

    Json Response

--- server.go
+++ server.go
@@ -4,15 +4,30 @@ import(
 	"github.com/gorilla/mux"
 	"net/http"
 	"log"
+	"encoding/json"
 )
 
+type Response struct {
+	Message string `json:"user_name"`
+}
+
+func HolaMundo(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("Hola Mundo\n"))
+}
+
+func HolaJson(w http.ResponseWriter, r *http.Request){
+	response := Response{"Estes es un mensaje en JSON"}
+	json.NewEncoder(w).Encode(response)
+}
+
 func HomeHandler(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Hola Mundo\n"))
+	
 }
 
 func main() {
 	mux := mux.NewRouter()
-	mux.HandleFunc("/", HomeHandler)
+	mux.HandleFunc("/", HolaMundo)
+	mux.HandleFunc("/json", HolaJson)
 	
 	http.Handle("/", mux)
 	log.Println("El servidor se encuentra a la escucha en el puerto 8000")
